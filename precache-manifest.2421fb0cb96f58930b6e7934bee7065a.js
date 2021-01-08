e433b86326fcc4c63a47c1a1e911ac152053db43
Kibatensai <lenutd@yandex.ru>
2021-01-08 22:29:31 +0300

    Updates

---
+++ precache-manifest.2421fb0cb96f58930b6e7934bee7065a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5881d686717563c194c660e303f711",
+    "url": "/incubator_ignat_challenges/index.html"
+  },
+  {
+    "revision": "c18ccfe32c01f79db702",
+    "url": "/incubator_ignat_challenges/static/css/main.1ec330bd.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/incubator_ignat_challenges/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/incubator_ignat_challenges/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18ccfe32c01f79db702",
+    "url": "/incubator_ignat_challenges/static/js/main.b6ad16e8.chunk.js"
+  },
+  {
+    "revision": "d54732af48ad2cfd9e12",
+    "url": "/incubator_ignat_challenges/static/js/runtime-main.c3a332af.js"
+  }
+]);
