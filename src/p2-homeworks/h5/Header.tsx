add48efd4ddc3b59417474b53ce35f747ba26b47
Kibatensai <lenutd@yandex.ru>
2021-01-09 17:20:39 +0300

    Added new components and paths for routing

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { PATH } from "./Routes";
 
 function Header() {
     return (
         <div>
             // add NavLinks
-
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>Junior +</NavLink>
         </div>
     );
 }
